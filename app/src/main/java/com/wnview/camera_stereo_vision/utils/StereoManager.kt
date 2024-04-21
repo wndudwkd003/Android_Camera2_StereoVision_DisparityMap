8c15cc3a8c8c3b0ab1843f13a17ed65229623d4d
wndudwkd003 <ymail3@naver.com>
2024-04-21 19:43:41 +0900

    2024-04-18 Disparity Map 생성
    - 와이드, 울트라 와이드 fov 기준으로 비슷하게만듦
    - streo sgbm으로 disparitymap 생성

--- app/src/main/java/com/wnview/camera_stereo_vision/utils/StereoManager.kt
+++ app/src/main/java/com/wnview/camera_stereo_vision/utils/StereoManager.kt
@@ -9,7 +9,7 @@ object StereoManager {
         val minDisparity = 0
         // disparity(시차) 최소 값 설정 (일반적으로 0)
 
-        val numDisparities = 16 * 3
+        val numDisparities = 16 * 2
         // disparity 값의 총 범위 설정, 16의 배수로 설정하는 것이 일반적
         // numDisparities = 최대 disparity - 최소 disparity
 
@@ -27,7 +27,7 @@ object StereoManager {
         // StereoSGBM 알고리즘의 P2 파라미터 설정
         // 인접한 픽셀 간 disparity의 차이에 대한 가중치 파라미터
 
-        val disp12MaxDiff = 1
+        val disp12MaxDiff = 5
         // 유효 disparity 차이의 최대 값 설정
         // disp12MaxDiff는 이미지 쌍 간 disparity 맵에서 정합(matching)이 잘못된 픽셀의 최대 허용 값
 
@@ -36,11 +36,11 @@ object StereoManager {
         // disparity가 가장 유일하다고 판단되는 픽셀의 비율
         // 높은 값은 더 엄격한 제약 조건을 의미하며, 낮은 값은 덜 엄격한 제약 조건을 의미
 
-        val speckleWindowSize = 0
+        val speckleWindowSize = 5
         // 스펙클 제거를 위한 윈도우 크기 설정
         // 스펙클 제거를 수행할 윈도우의 크기 (0이면 스펙클 제거가 비활성화됨)
 
-        val speckleRange = 2
+        val speckleRange = 5
         // 스펙클 제거를 위한 픽셀 간 disparity 차이 설정
         // 스펙클 제거 시, 인접 픽셀 간 disparity 값의 차이에 대한 임계값
 
@@ -48,7 +48,7 @@ object StereoManager {
         // 프리 필터(capacity) 설정
         // disparity map을 계산하기 전에 이미지에 적용되는 전처리 필터의 강도
 
-        val mode = StereoSGBM.MODE_SGBM
+        val mode = StereoSGBM.MODE_SGBM_3WAY
         // StereoSGBM 알고리즘의 모드 설정
 
         // StereoSGBM 생성
